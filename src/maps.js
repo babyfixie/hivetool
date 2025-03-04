2df4a46ff674bda8dca2d408792fc6e9479b6239
Arseniy <arsenyzaretsky182@gmail.com>
2025-03-04 14:38:53 +0200

    fix images 404

--- src/maps.js
+++ src/maps.js
@@ -18,63 +18,17 @@ async function fetchMaps() {
   });
 
   filteredMaps.forEach(map => {
-    /* if (gamemode === 'murder') {
-      if (map.name === 'Backrooms') {
-        map.image = 'https://cdn.playhive.com/maps/mm_backrooms.jpg';
-      }
-
-      if (map.name === 'Office') {
-        map.image = 'https://cdn.playhive.com/maps/mm_office.jpg';
-      }
-    }
-
-    if (gamemode === 'bed') {
-      if (map.name === 'Monster Labs') {
-        map.image = './img/no-image.jpg';
-      }
-    }
-
-    if (gamemode === 'sky') {
-      if (
-        map.name === 'Pinecone Point' ||
-        map.name === 'Wintergrove' ||
-        map.name === 'Jungle'
-      ) {
-        map.image = './img/no-image.jpg';
-      }
-    }
-
-    if (gamemode === 'wars') {
-      if (map.name === 'Abandoned') {
-        map.image = './img/no-image.jpg';
-      }
-    } */
-    const mapImages = {
-      murder: {
-        Backrooms: 'https://cdn.playhive.com/maps/mm_backrooms.jpg',
-        Office: 'https://cdn.playhive.com/maps/mm_office.jpg',
-      },
-      bed: {
-        'Monster Labs': './img/no-image.jpg',
-      },
-      sky: {
-        'Pinecone Point': './img/no-image.jpg',
-        Wintergrove: './img/no-image.jpg',
-        Jungle: './img/no-image.jpg',
-      },
-      wars: {
-        Abandoned: './img/no-image.jpg',
-      },
-    };
-
-    if (mapImages[gamemode] && mapImages[gamemode][map.name]) {
-      map.image = mapImages[gamemode][map.name];
-    }
+    const mapImage = map.image || './img/no-image.jpg';
 
     const cardHTML = `
       <div class="col-md-4 mb-4">
         <div class="card">
-          <img src="${map.image}" class="card-img-top" alt="${map.name}" />
+          <img 
+            src="${mapImage}" 
+            class="card-img-top" 
+            alt="${map.name}" 
+            onerror="this.onerror=null; this.src='./img/no-image.jpg';"
+          />
           <div class="card-body">
             <h5 class="card-title">${map.name}</h5>
             <p class="card-text">Variant: ${map.variant}</p>
